0aec48b69b191bb6bd0f9e08bb9e24657897b09f
henry-herrmann <henryherrmann.tv@gmail.com>
2022-05-06 15:39:22 +0200

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 const express = require("express");
-const fs = require("fs");
 
 const app = express();
 
-app.get("/", (req, res) =>{
-    fs.readFile("./public/index.html", "utf-8", function(err, data) {
-        if(err) return res.status(503).send("Service unavailable.");
-
-        res.send(data);
-    })
-})
-
-app.get("*", (req, res) =>{
-    res.send(404).send("Not found");
-})
+app.use(express.static("public"));
 
 app.listen(8090, () =>{
     console.log("[INFO] Portfolio webserver started.")
